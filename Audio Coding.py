c63b009496b53dae1b438c0ebba22ef0b35ee289
Zangorth <Samuel.A.York.1@gmail.com>
2021-07-18 09:00:39 -0400

    Re-parallezied in smarter way to save CPU and time

--- Audio Coding.py
+++ Audio Coding.py
@@ -1,6 +1,10 @@
+from sqlalchemy import create_engine
+from pydub import AudioSegment
+import multiprocessing as mp
 import pyodbc as sql
 import pandas as pd
 import warnings
+import urllib
 import sys
 import os
 
@@ -17,6 +21,9 @@ con = sql.connect('''DRIVER={ODBC Driver 17 for SQL Server};
 query = 'SELECT DISTINCT id FROM RAMSEY.dbo.AudioCoding'
 completed = pd.read_sql(query, con)
 completed = completed['id'].tolist()
+
+columns = pd.read_sql('Select TOP 1 * FROM RAMSEY.dbo.AudioCoding', con)
+columns = columns.columns
 con.close()
 
 incomplete = os.listdir()
@@ -25,4 +32,25 @@ incomplete = [f'{file}.mp3' for file in incomplete]
 
 for i in range(len(incomplete)):
     print(f'Progress: {i}/{len(incomplete)}')
-    extract_audio(incomplete[i])
+    
+    sound = AudioSegment.from_file(incomplete[i])
+    iterables = [[int(incomplete[i].replace('.mp3', '')), cut, sound[cut*1000:cut*1000+3000]] for cut in range(int(round(len(sound)/1000, 0)))]
+    
+    with mp.Pool(10) as pool:
+        out = pool.map(extract_audio, iterables)
+        
+    out = [ls for ls in out if ls != []]
+    panda = pd.DataFrame(out, columns=columns)
+        
+    conn_str = (
+        r'Driver={SQL Server};'
+        r'Server=ZANGORTH\HOMEBASE;'
+        r'Database=RAMSEY;'
+        r'Trusted_Connection=yes;'
+    )
+    con = urllib.parse.quote_plus(conn_str)
+    
+    engine = create_engine(f'mssql+pyodbc:///?odbc_connect={con}')
+    
+    panda.to_sql(name='AudioCoding', con=engine, schema='dbo', if_exists='append', index=False)
+    
--- extract_audio.py
+++ extract_audio.py
@@ -1,76 +1,39 @@
-from sqlalchemy import create_engine
-from pydub import AudioSegment
-import pandas as pd
 import numpy as np
 import warnings
 import librosa
-import urllib
+
+warnings.filterwarnings('ignore')
+
 
 class ParameterError(Exception):
     pass
 
-warnings.filterwarnings('ignore')
-
-def extract_audio(file):
-    sound = AudioSegment.from_file(file)
-    first, cut = True, 0
+def extract_audio(sound):
     
-    while cut <= len(sound):
-        out = sound[cut:cut+3000]
+    try:
+        file = sound[0]
+        cut = sound[1]
+        sound = sound[2]
         
-        try:
-            y, rate = librosa.load(out.export(format='wav'), res_type='kaiser_fast')
-            mfccs = np.mean(librosa.feature.mfcc(y, rate, n_mfcc=40).T,axis=0)
-            stft = np.abs(librosa.stft(y))
-            chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=rate).T,axis=0)
-            mel = np.mean(librosa.feature.melspectrogram(y, sr=rate).T,axis=0)
-            contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=rate).T,axis=0)
-            tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(y), sr=rate).T,axis=0)
-            
-            features = list(mfccs) + list(chroma) + list(mel) + list(contrast) + list(tonnetz)
-            features = [float(f) for f in features]
-            features = [int(file.replace('.mp3', '')), cut/1000] + features
-            
-            if first:
-                temp = pd.DataFrame(features).T
-                temp.columns = (['id', 'second'] + [f'mfccs_{i}' for i in range(len(mfccs))] + 
-                                [f'chroma_{i}' for i in range(len(chroma))] + [f'mel_{i}' for i in range(len(mel))] +
-                                [f'contrast_{i}' for i in range(len(contrast))] + [f'tonnetz_{i}' for i in range(len(tonnetz))])
-                
-                panda = temp.copy()
-                first = False
-                
-            else:
-                temp = pd.DataFrame(features).T
-                temp.columns = (['id', 'second'] + [f'mfccs_{i}' for i in range(len(mfccs))] + 
-                                [f'chroma_{i}' for i in range(len(chroma))] + [f'mel_{i}' for i in range(len(mel))] +
-                                [f'contrast_{i}' for i in range(len(contrast))] + [f'tonnetz_{i}' for i in range(len(tonnetz))])
-                panda = panda.append(temp, ignore_index=True, sort=False)
-                
-        except ValueError:
-            pass
+        y, rate = librosa.load(sound.export(format='wav'), res_type='kaiser_fast')
+        mfccs = np.mean(librosa.feature.mfcc(y, rate, n_mfcc=40).T,axis=0)
+        stft = np.abs(librosa.stft(y))
+        chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=rate).T,axis=0)
+        mel = np.mean(librosa.feature.melspectrogram(y, sr=rate).T,axis=0)
+        contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=rate).T,axis=0)
+        tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(y), sr=rate).T,axis=0)
         
-        except ParameterError:
-            pass
+        features = list(mfccs) + list(chroma) + list(mel) + list(contrast) + list(tonnetz)
+        features = [float(f) for f in features]
+        features = [file, cut] + features
         
-        except Exception:
-            pass
+    except ValueError:
+        features = []
         
-        
-        cut += 1000
-        
-    panda['id'] = panda['id'].astype(int)
-        
-    conn_str = (
-        r'Driver={SQL Server};'
-        r'Server=ZANGORTH\HOMEBASE;'
-        r'Database=RAMSEY;'
-        r'Trusted_Connection=yes;'
-    )
-    con = urllib.parse.quote_plus(conn_str)
+    except ParameterError:
+        features = []
     
-    engine = create_engine(f'mssql+pyodbc:///?odbc_connect={con}')
+    except Exception:
+        features = []
     
-    panda.to_sql(name='AudioCoding', con=engine, schema='dbo', if_exists='append', index=False)
-    
-    return None
+    return features
