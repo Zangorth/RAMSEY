b38cf57e3cd9639434b72174cc9c59ac4648b64f
Zangorth <Samuel.A.York.1@gmail.com>
2021-08-01 09:54:12 -0400

    Updated semi-supervision procedure

--- Supervised Voice Recognition.py
+++ Supervised Voice Recognition.py
@@ -24,7 +24,6 @@ con = sql.connect('''DRIVER={ODBC Driver 17 for SQL Server};
 query = '''
 SELECT *
 FROM RAMSEY.dbo.predictions
-WHERE id IN (232, 5059, 4188, 5061, 1372, 321, 1359, 391)
 '''
 semi = pd.read_sql(query, con)
 con.close()
@@ -46,7 +45,8 @@ con.close()
 full = True
 
 if full:
-    samples = pd.DataFrame({'id': [21, 53, 59, 83, 103, 114, 261, 276, 315, 321, 336, 337, 339, 375, 378],
+    samples = pd.DataFrame({'id': [21, 53, 59, 83, 103, 114, 232, 261, 276, 315, 321, 336, 337, 339, 375, 378, 391,
+                                   1359, 1372, 4188, 5059, 5061],
                             'second': 0})
     start = 0
 
@@ -59,7 +59,7 @@ for i in range(len(samples)):
     sample = samples['id'][i]
     second = samples['second'][i]
     
-    upload = pd.DataFrame(columns=['id', 'second', 'speaker'])
+    upload = pd.DataFrame(columns=['id', 'second', 'speaker', 'source'])
     
     sound = AudioSegment.from_file(f'C:\\Users\\Samuel\\Audio\\Audio Full\\{sample}.mp3')
     sound = sound if full else sound[second*1000-10000:second*1000+20000]
@@ -104,77 +104,43 @@ for i in range(len(samples)):
 ##############################
 # Semi - Supervised Learning #
 ##############################
+upload = pd.DataFrame(columns=['id', 'second', 'speaker', 'source'])
 
-for sample in set(semi['id']):
-    supervision = semi.loc[semi.id == sample].sort_values('second').reset_index(drop=True)
-    second = supervision.second.min()
+samples = semi.sample(500).reset_index(drop=True)
+
+for i in range(len(samples)):
+    sample = int(samples['id'][i])
+    second = samples['second'][i]
+    label = samples['speaker'][i]
     
     sound = AudioSegment.from_file(f'C:\\Users\\Samuel\\Audio\\Audio Full\\{sample}.mp3')
-    sound = sound[second*1000:]
+    lead = sound[second*1000-5000: second*1000+1000]
+    sound = sound[second*1000:second*1000+1000]
     
-    for cut in range(0, len(sound), 1000):
-        label = supervision.loc[supervision.second == second, 'speaker'].item()
+    speaker = 0
         
-        play(sound[cut:cut+1000])
-        speaker = input(f'Speaker {label}? ')
-        
-        if speaker != '':
-            supervision.loc[supervision.second == second, 'speaker'] = speaker
-            print(f'({second}/{len(sound)/1000}) Updated: Speaker = {speaker}')
-        
-        second += 1
-        
-    supervision['source'] = 'semi'
-    supervision = supervision[['id', 'second', 'speaker', 'source']]
+    while speaker == 0:
+        play(sound)
+
+        speaker = input(f'({upload.index.max()}/{len(samples)}) Speaker {label}? ')
+        speaker = 0 if speaker == '0' else speaker
+
+        if speaker == '':
+            speaker = label
+        elif str(speaker).lower() == 'lead':
+            play(lead)
+            speaker = 0
+            
+    upload = upload.append(pd.DataFrame({'id': [sample], 'second': [second], 
+                                         'speaker': [speaker], 'source': 'semi'}),
+                           ignore_index=True, sort=False)
     
-    conn_str = (
+conn_str = (
         r'Driver={SQL Server};'
         r'Server=ZANGORTH\HOMEBASE;'
         r'Database=RAMSEY;'
         r'Trusted_Connection=yes;'
     )
-    con = urllib.parse.quote_plus(conn_str)
-    engine = create_engine(f'mssql+pyodbc:///?odbc_connect={con}')
-    supervision.to_sql(name='AudioTraining', con=engine, schema='dbo', if_exists='append', index=False)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+con = urllib.parse.quote_plus(conn_str)
+engine = create_engine(f'mssql+pyodbc:///?odbc_connect={con}')
+upload.to_sql(name='AudioTraining', con=engine, schema='dbo', if_exists='append', index=False)
