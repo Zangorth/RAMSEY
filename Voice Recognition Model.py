e27be85a8f9a9d9a51a9253d3cd4b15b102a3e1a
Zangorth <Samuel.A.York.1@gmail.com>
2021-08-13 07:38:37 -0400

    Updated with Days of Week feature to (hopefully) improve fit

--- Voice Recognition Model.py
+++ Voice Recognition Model.py
@@ -1,3 +1,4 @@
+from sklearn.linear_model import LogisticRegression
 from sklearn.exceptions import ConvergenceWarning
 from sklearn.decomposition import PCA
 from matplotlib import pyplot as plt
@@ -6,7 +7,6 @@ from sklearn.cluster import KMeans
 from sklearn import preprocessing
 from pydub.playback import play
 from pydub import AudioSegment
-from datetime import datetime
 import seaborn as sea
 import pyodbc as sql
 from torch import nn
@@ -22,7 +22,6 @@ import sys
 sys.path.append(r'C:\Users\Samuel\Google Drive\Portfolio\Ramsey')
 import ramsey_helpers as RH
 
-year_continuous = False
 full = False
 optimize = True
 
@@ -41,42 +40,44 @@ con = sql.connect('''DRIVER={ODBC Driver 17 for SQL Server};
 query = '''
 SET NOCOUNT ON
 
-SELECT id, [second]
+SELECT id, [second], [source]
 INTO #speakers
 FROM RAMSEY.dbo.AudioTraining
 UNION
-SELECT id, [second]-1
+SELECT id, [second]-1, [source]
 FROM RAMSEY.dbo.AudioTraining
 UNION
-SELECT id, [second]-2
+SELECT id, [second]-2, [source]
 FROM RAMSEY.dbo.AudioTraining
 UNION
-SELECT id, [second]-3
+SELECT id, [second]-3, [source]
 FROM RAMSEY.dbo.AudioTraining
 UNION
-SELECT id, [second]-4
+SELECT id, [second]-4, [source]
 FROM RAMSEY.dbo.AudioTraining
 UNION
-SELECT id, [second]-5
+SELECT id, [second]-5, [source]
 FROM RAMSEY.dbo.AudioTraining
 UNION
-SELECT id, [second]+1
+SELECT id, [second]+1, [source]
 FROM RAMSEY.dbo.AudioTraining
 UNION
-SELECT id, [second]+2
+SELECT id, [second]+2, [source]
 FROM RAMSEY.dbo.AudioTraining
 UNION
-SELECT id, [second]+3
+SELECT id, [second]+3, [source]
 FROM RAMSEY.dbo.AudioTraining
 UNION
-SELECT id, [second]+4
+SELECT id, [second]+4, [source]
 FROM RAMSEY.dbo.AudioTraining
 UNION
-SELECT id, [second]+5
+SELECT id, [second]+5, [source]
 FROM RAMSEY.dbo.AudioTraining
 
 SELECT speaker , YEAR(publish_date) AS 'publish_year', 
-    [source], code.*
+    DATENAME(DW, publish_date) AS 'dow', 
+    CONCAT(YEAR(publish_date),'-',  DATENAME(DW, publish_date)) AS 'interaction',
+    #speakers.[source], code.*
 FROM #speakers
 LEFT JOIN RAMSEY.dbo.AudioTraining AS train
     ON #speakers.id = train.id
@@ -87,6 +88,7 @@ LEFT JOIN RAMSEY.dbo.AudioCoding AS code
 LEFT JOIN RAMSEY.dbo.metadata AS meta
     ON #speakers.id = meta.id
 WHERE code.id IS NOT NULL
+    AND #speakers.[source] != 'test'
 ORDER BY id, [second]
 '''
 
@@ -95,6 +97,8 @@ train_audio = pd.read_sql(query, con)
 if full:
     query = '''
     SELECT YEAR(publish_date) AS 'publish_year', 
+        DATENAME(DW, publish_date) AS 'dow', 
+        CONCAT(YEAR(publish_date),'-',  DATENAME(DW, publish_date)) AS 'interaction',
         audio.*
     FROM RAMSEY.dbo.AudioCoding AS audio
     RIGHT JOIN RAMSEY.dbo.metadata AS meta
@@ -102,6 +106,7 @@ if full:
     WHERE meta.seconds <= 900
     '''
     audio = pd.read_sql(query, con)
+    audio = audio.dropna().reset_index(drop=True)
 
 con.close()
 
@@ -116,107 +121,76 @@ train_audio = train_audio.drop('source', axis=1)
 mapped = train_audio[['y', 'speaker']].drop_duplicates().reset_index(drop=True)
 mapped = mapped.loc[mapped.y != -1].sort_values('y').reset_index(drop=True)
 
-if year_continuous:
-    years = pd.DataFrame(datetime.today().year - train_audio.publish_year)
-    years['publish_year_2'] = years.publish_year ** 2
-    years['publish_year_3'] = years.publish_year ** 3
-    years.columns = ['year1', 'year2', 'year3']
-    
-    x = train_audio.drop(['id', 'second', 'speaker', 'y', 'publish_year'], axis=1)
-    x = x.merge(years, left_index=True, right_index=True)
-    
-    save_cols = x.columns
-    
-    scaler = preprocessing.StandardScaler().fit(x)
-    x = pd.DataFrame(scaler.transform(x), columns=save_cols)
-    
-    pca = PCA(4)
-    pca = np.argmax(pca.fit_transform(x), axis=1)
-    
-    km = KMeans(6)
-    km = np.argmax(km.fit_transform(x), axis=1)
-    
-    x = train_audio[['id']].merge(x, left_index=True, right_index=True)
-    x['pca'] = pca
-    x['km'] = km
 
-else:
-    years = pd.get_dummies(train_audio['publish_year'])
-    x = train_audio.drop(['id', 'second', 'speaker', 'y', 'publish_year'], axis=1)
-    
-    save_cols = x.columns
-    
-    scaler = preprocessing.StandardScaler().fit(x)
-    x = pd.DataFrame(scaler.transform(x), columns=save_cols)
-    
-    pca = PCA(4)
-    pca = np.argmax(pca.fit_transform(x), axis=1)
-    
-    km = KMeans(6)
-    km = np.argmax(km.fit_transform(x), axis=1)
-    
-    x = train_audio[['id']].merge(x, left_index=True, right_index=True)
-    x = x.merge(years, left_index=True, right_index=True)
-    x['pca'] = pca
-    x['km'] = km
+years = pd.get_dummies(train_audio['publish_year'])
+years.columns = [str(col) for col in years.columns]
+
+days = pd.get_dummies(train_audio['dow'])
+interaction = pd.get_dummies(train_audio['interaction'])
+
+exclude = list(years.columns) + list(days.columns) + list(interaction.columns)
+
+x = train_audio.drop(['id', 'second', 'speaker', 'y', 'publish_year', 'dow', 'interaction'], axis=1)
+save_cols = x.columns
+
+scaler = preprocessing.StandardScaler().fit(x)
+x = pd.DataFrame(scaler.transform(x), columns=save_cols)
+
+pca = PCA(4)
+pca = np.argmax(pca.fit_transform(x), axis=1)
+
+km = KMeans(6)
+km = np.argmax(km.fit_transform(x), axis=1)
+
+x = train_audio[['id']].merge(x, left_index=True, right_index=True)
+x = x.merge(years, left_index=True, right_index=True)
+x = x.merge(days, left_index=True, right_index=True)
+x = x.merge(interaction, left_index=True, right_index=True)
+x['pca'] = pca
+x['km'] = km
 
 if full:
-    if year_continuous:
-        years = pd.DataFrame(datetime.today().year - audio.publish_year)
-        years['publish_year_2'] = years.publish_year ** 2
-        years['publish_year_3'] = years.publish_year ** 3
-        years.columns = ['year1', 'year2', 'year3']
-        
-        transform = audio.drop(['id', 'second', 'publish_year'], axis=1).merge(years, left_index=True, right_index=True)
-        
-        save_cols = transform.columns
-        
-        transform = pd.DataFrame(scaler.transform(transform), columns=save_cols)
-        
-        pca = PCA(4)
-        pca = np.argmax(pca.fit_transform(x), axis=1)
-        
-        km = KMeans(6)
-        km = np.argmax(km.fit_transform(x), axis=1)
-        
-        audio = audio[['id', 'second']].merge(transform, right_index=True, left_index=True)
-        audio['pca'] = pca
-        audio['km'] = km
-            
+    transform = audio.drop(['id', 'second', 'publish_year'], axis=1)
+    save_cols = transform.columns
     
-    else:
-        transform = audio.drop(['id', 'second', 'publish_year'], axis=1)
-        save_cols = transform.columns
-        
-        transform = pd.Dataframe(scaler.transform(transform), columns=save_cols)
-        
-        pca = PCA(4)
-        pca = np.argmax(pca.fit_transform(x), axis=1)
-        
-        km = KMeans(6)
-        km = np.argmax(km.fit_transform(x), axis=1)
-        
-        years = pd.get_dummies(audio['publish_year'])
-        
-        audio = audio[['id', 'second']].merge(transform, right_index=True, left_index=True)
-        audio = audio.merge(years, right_index=True, left_index=True).dropna().reset_index(drop=True)
-        audio['pca'] = pca
-        audio['km'] = km
+    transform = pd.DataFrame(scaler.transform(transform), columns=save_cols)
+    
+    pca = PCA(4)
+    pca = np.argmax(pca.fit_transform(transform), axis=1)
+    
+    km = KMeans(6)
+    km = np.argmax(km.fit_transform(transform), axis=1)
+    
+    years = pd.get_dummies(audio['publish_year'])
+    years.columns = [str(col) for col in years.columns]
+    
+    days = pd.get_dummies(audio['dow'])
+    interaction = pd.get_dummies(audio['interaction'])
+    
+    audio = audio[['id', 'second']].merge(transform, right_index=True, left_index=True)
+    audio = audio.merge(years, right_index=True, left_index=True)
+    audio = audio.merge(days, right_index=True, left_index=True)
+    audio = audio.merge(interaction, right_index=True, left_index=True)
+    
+    audio['pca'] = pca
+    audio['km'] = km
+    
+    audio = audio.dropna().reset_index(drop=True)
 
 ####################
 # Optimize Network #
 ####################
-calls, max_f1 = 500, []
+calls, max_f1 = 25, []
 max_shift = 5
 space = [
-    skopt.space.Categorical(['nn'], name='model'),
+    skopt.space.Categorical(['logit'], name='model'),
     skopt.space.Integer(0, max_shift, name='lags'),
     skopt.space.Integer(0, max_shift, name='leads'),
     skopt.space.Integer(0, 1, name='pca'),
     skopt.space.Integer(0, 1, name='km'),
-    #skopt.space.Integer(50, 500, name='n_samples'),
-    #skopt.space.Integer(50, 500, name='n_estimators'),
-    #skopt.space.Real(0.0001, 0.1, name='lr_gbc', prior='log-uniform'),
+    skopt.space.Integer(50, 500, name='n_samples'),
+    skopt.space.Integer(50, 500, name='n_estimators'),
+    skopt.space.Real(0.0001, 0.1, name='lr_gbc', prior='log-uniform'),
     skopt.space.Integer(1, 5, name='layers'),
     skopt.space.Integer(1, 100, name='epochs'),
     skopt.space.Real(0.0001, 0.2, name='drop', prior='log-uniform'),
@@ -228,10 +202,14 @@ space = space + [skopt.space.Integer(2**2, 2**10, name=f'transform_{i}') for i i
 
 tracker, i = [], 0
 
+lags, leads, km, pca = 3, 3, 1, 0
+drop, lr_nn, epochs, layers = 0.001, 0.001, 20, 4
+transforms = [132, 64, 32, 16]
+
 @skopt.utils.use_named_args(space)
 def net(model, lags, leads, km, pca, n_samples=None, n_estimators=None, lr_gbc=None, max_depth=None, 
         drop=None, lr_nn=None, epochs=None, layers=None, **kwargs):
-    lx = RH.shift(x, 'id', lags, leads, exclude=years.columns)
+    lx = RH.shift(x, 'id', lags, leads, exclude=exclude)
     lx = lx.loc[y != -1].drop('id', axis=1).dropna()
     ly = y.loc[lx.index]
     
@@ -281,7 +259,7 @@ def net(model, lags, leads, km, pca, n_samples=None, n_estimators=None, lr_gbc=N
     return (- 1.0 * f1)
         
 if optimize:
-    result = skopt.forest_minimize(net, space, acq_func='PI', n_initial_points=25, n_calls=calls, n_jobs=-1)
+    result = skopt.forest_minimize(net, space, acq_func='PI', n_initial_points=10, n_calls=calls)
     
     features = {'lags': 1, 'leads': 2, 'pca': 3, 'km': 4, 'layers': 5, 'epochs': 6, 'drop': 7, 'lr': 8}
     
@@ -295,12 +273,14 @@ if optimize:
     
     max_f1.append(result)
     print(f'Max F1: {result.fun}')
-    print(f'Parameters: {result.x}')
     
     results = {'model': result.x[0], 'lags': result.x[1], 'leads': result.x[2], 
-               'pca': result.x[3], 'km': result.x[4], 
-               'layers': result.x[5], 'epochs': result.x[6], 'drop': result.x[7],
-               'lr_nn': result.x[8], 'transform': [result.x[i] for i in range(9, len(result.x))]}
+               'pca': result.x[3], 'km': result.x[4], 'n_samples': result.x[5],
+               'n_estimators': result.x[6], 'lr_gbc': result.x[7], 
+               'layers': result.x[8], 'epochs': result.x[9], 'drop': result.x[10],
+               'lr_nn': result.x[11], 'transform': [result.x[i] for i in range(12, len(result.x))]}
+    
+    print(results)
     
     pickle.dump(results, open('results.pkl', 'wb'))
 else:
@@ -310,7 +290,7 @@ else:
 ####################
 # Validate Network #
 ####################
-x = RH.shift(x, 'id', results['lags'], results['leads'], exclude=years.columns)
+x = RH.shift(x, 'id', results['lags'], results['leads'], exclude=exclude)
 x = x.loc[y != -1].drop('id', axis=1).dropna()
 y = y.loc[x.index]
 
@@ -324,12 +304,18 @@ test_audio = train_audio.iloc[x.index]
 
 x, y = x.reset_index(drop=True), y.reset_index(drop=True)
 
-
-f1 = RH.cv_nn(x, y, results['transform'], results['drop'], results['lr_nn'], 
-              results['epochs'], results['layers'], prin=True, wrong=True)
-
-f1['pred'] = f1.merge(mapped, how='left', left_on='pred', right_on='y')['speaker']
-f1['real'] = f1.merge(mapped, how='left', left_on='real', right_on='y')['speaker']
+if results['model'] == 'logit':
+    avg = RH.cv_logit(x, y, avg=True)
+    print(avg)
+    
+elif results['model'] == 'nn':
+    avg = RH.cv_nn(x, y, results['transform'], results['drop'], results['lr_nn'], 
+                   results['epochs'], results['layers'], avg=True)
+    avg = avg.merge(mapped, left_on='speaker_id', right_on='y')[['speaker', 'f1']]
+    print(avg)
+    
+    f1 = RH.cv_nn(x, y, results['transform'], results['drop'], results['lr_nn'], 
+                   results['epochs'], results['layers'], wrong=True)
 
 
 ##################################
@@ -371,40 +357,13 @@ if check:
             else:
                 print(f'UPDATE: {sample} {cut/1000}: {label} -> {speaker}')
 
-#################
-# Train Network #
-#################
-torch.cuda.empty_cache()
-x, y = torch.from_numpy(x.values).to(device), torch.from_numpy(y.values).to(device)
-train_set = [(x[i], y[i]) for i in range(len(y))]
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True)
+###############
+# Predictions #
+###############
+predictions = pd.DataFrame(columns=['speaker'])
 
-loss_function = nn.CrossEntropyLoss()
-discriminator = RH.Discriminator(x.shape[1], results['transform'], results['drop'], 10).to(device)
-optim = torch.optim.Adam(discriminator.parameters(), lr=results['lr_nn'])
-
-for epoch in range(results['epochs']):
-    for i, (inputs, targets) in enumerate(train_loader):
-        discriminator.zero_grad()
-        yhat = discriminator(inputs.float())
-        loss = loss_function(yhat, targets.long())
-        loss.backward()
-        optim.step()
-
-discriminator.eval()
-
-#########################
-# Predicting Full Audio #
-#########################
-
-# GPU barely too small to handle all the data, so just iterating over it
-predictions = pd.DataFrame(columns=['speaker', 'confidence'])
-i = 0
-while i <= len(audio):
-    print(f'{i}:{i+100000} / {len(audio)}')
-    
-    audio_x = audio.iloc[i:i+100000]
-    audio_x = RH.shift(audio_x, 'id', results['lags'], results['leads'], exclude=['second'] + list(years.columns))
+if results['model'] == 'logit':
+    audio_x = RH.shift(audio, 'id', results['lags'], results['leads'], exclude=['second'] + exclude)
     audio_x = audio_x.dropna()
     
     if results['pca'] == 0:
@@ -413,16 +372,60 @@ while i <= len(audio):
     if results['km'] == 0:
         audio_x = audio_x.drop('km', axis=1)
     
-    audio_x = torch.from_numpy(audio_x.drop(['id', 'second'], axis=1).values).float()
+    discriminator = LogisticRegression(max_iter=500, fit_intercept=False)
+    discriminator.fit(x, y)
     
-    append = pd.DataFrame({'speaker_id': np.argmax(discriminator(audio_x.to(device)).cpu().detach().numpy(), axis=1),
-                           'confidence': np.max(discriminator(audio_x.to(device)).cpu().detach().numpy(), axis=1)})
-    append = append.merge(mapped, how='left', left_on='speaker_id', right_on='y')[['speaker', 'confidence']]
+    i = 0
+    while i <= len(audio_x):
+        print(f'{i}:{i+100000} / {len(audio)}')
+        
+        discriminator.predict(audio_x.drop(['id', 'second'], axis=1).iloc[0:100000])
     
-    predictions = predictions.append(append, ignore_index=True, sort=False)
+        predictions = discriminator.predict(audio_x.drop(['id', 'second'], axis=1))
+
+elif results['model'] == 'nn':
+    torch.cuda.empty_cache()
+    x, y = torch.from_numpy(x.values).to(device), torch.from_numpy(y.values).to(device)
+    train_set = [(x[i], y[i]) for i in range(len(y))]
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True)
     
-    i+=100000
+    loss_function = nn.CrossEntropyLoss()
+    discriminator = RH.Discriminator(x.shape[1], results['transform'], results['drop'], 10).to(device)
+    optim = torch.optim.Adam(discriminator.parameters(), lr=results['lr_nn'])
     
+    for epoch in range(results['epochs']):
+        for i, (inputs, targets) in enumerate(train_loader):
+            discriminator.zero_grad()
+            yhat = discriminator(inputs.float())
+            loss = loss_function(yhat, targets.long())
+            loss.backward()
+            optim.step()
+    
+    discriminator.eval()
+    
+    i = 0
+    while i <= len(audio):
+        print(f'{i}:{i+100000} / {len(audio)}')
+        
+        audio_x = audio.iloc[i:i+100000]
+        audio_x = RH.shift(audio_x, 'id', results['lags'], results['leads'], exclude=['second'] + list(years.columns))
+        audio_x = audio_x.dropna()
+        
+        if results['pca'] == 0:
+            audio_x = audio_x.drop('pca', axis=1)
+            
+        if results['km'] == 0:
+            audio_x = audio_x.drop('km', axis=1)
+        
+        audio_x = torch.from_numpy(audio_x.drop(['id', 'second'], axis=1).values).float()
+        
+        append = pd.DataFrame({'speaker_id': np.argmax(discriminator(audio_x.to(device)).cpu().detach().numpy(), axis=1),
+                               'confidence': np.max(discriminator(audio_x.to(device)).cpu().detach().numpy(), axis=1)})
+        append = append.merge(mapped, how='left', left_on='speaker_id', right_on='y')[['speaker', 'confidence']]
+        
+        predictions = predictions.append(append, ignore_index=True, sort=False)
+        
+        i+=100000
     
 predictions = audio[['id', 'second']].merge(predictions, right_index=True, left_index=True, how='right')
 
