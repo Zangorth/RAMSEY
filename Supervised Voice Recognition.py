6bee5fda19179a97500e621d0cab8fdae68a0aae
Zangorth <Samuel.A.York.1@gmail.com>
2021-08-13 07:39:17 -0400

    Minor Updates

--- Supervised Voice Recognition.py
+++ Supervised Voice Recognition.py
@@ -19,7 +19,7 @@ SELECT AudioCoding.id, second, metadata.link
 FROM RAMSEY.dbo.AudioCoding
 LEFT JOIN RAMSEY.DBO.metadata
     ON AudioCoding.id = metadata.id
-WHERE YEAR(publish_date) >= 2020
+--WHERE YEAR(publish_date) >= 2020
 '''
 panda = pd.read_sql(query, con)
 
@@ -28,10 +28,12 @@ con = sql.connect('''DRIVER={ODBC Driver 17 for SQL Server};
                   Trusted_Connection=yes;''')
                   
 query = '''
-SELECT predictions.id, second, speaker, link
+SELECT CONVERT(INT, predictions.id) AS 'id', 
+    second, speaker, link
 FROM RAMSEY.dbo.predictions
 LEFT JOIN RAMSEY.DBO.metadata
     ON predictions.id = metadata.id
+--WHERE YEAR(publish_date) >= 2020
 '''
 semi = pd.read_sql(query, con)
 
@@ -51,40 +53,43 @@ con.close()
 #######################
 # Supervised Learning #
 #######################
-sam_size = 500
+sam_size = 6
 
 if supervise:
-    samples = panda.sample(sam_size).reset_index(drop=True)
-    samples = samples.merge(checked, how='left', on=['id', 'second'])
+    samples = panda.merge(checked, how='left', on=['id', 'second'])
+    samples = samples.sample(sam_size).reset_index(drop=True)
     samples = samples.loc[samples.found.isnull(), ['id', 'second', 'link']].reset_index(drop=True)
 
 else:
-    samples = semi.sample(sam_size).reset_index(drop=True)
-    samples = samples.merge(checked, how='left', on=['id', 'second'])
+    samples = semi.merge(checked, how='left', on=['id', 'second'])
+    samples = samples.sample(sam_size).reset_index(drop=True)
     samples = samples.loc[samples.found.isnull(), ['id', 'second', 'speaker', 'link']].reset_index(drop=True)
     
 
 for i in range(len(samples)):
     sample = samples['id'][i]
     second = samples['second'][i]
-    prediction = '' if supervise else f'{samples["speaker"][i]}?'
+    prediction = '' if supervise else f'{samples["speaker"][i]}? '
     
     upload = pd.DataFrame(columns=['id', 'second', 'speaker', 'source'])
-    
-    sound = AudioSegment.from_file(f'C:\\Users\\Samuel\\Audio\\Audio Full\\{sample}.mp3')
-    lead = sound[second*1000-3000: second*1000+3000]
-    sound = sound[second*1000:second*1000+1000]
         
     speaker = 0
     
     while speaker == 0:
+        sound = AudioSegment.from_file(f'C:\\Users\\Samuel\\Audio\\Audio Full\\{sample}.mp3')
+        lead = sound[second*1000-3000: second*1000+3000]
+        sound = sound[second*1000:second*1000+1000]
+        
         play(sound)
 
         speaker = input(f'({i+1}/{len(samples)}) Speaker: {prediction}')
         speaker = speaker.upper()
         speaker = 0 if speaker == '0' else speaker
 
-        if str(speaker).lower() == 'lead':
+        if str(speaker).lower() == '' and not supervise:
+            speaker = samples['speaker'][i]
+
+        elif str(speaker).lower() == 'lead':
             play(lead)
             speaker = 0
             
@@ -94,8 +99,7 @@ for i in range(len(samples)):
             speaker = 0
     
     
-    upload = pd.DataFrame({'id': [sample], 'second': [second], 'speaker': [speaker], 'source': '2020+'})
-    
+    upload = pd.DataFrame({'id': [sample], 'second': [second], 'speaker': [speaker], 'source': 'test'})
                 
     conn_str = (
         r'Driver={SQL Server};'
