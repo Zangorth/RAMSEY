5053748260b89a37976a9f0e092da62c8a2a9a93
Zangorth <Samuel.A.York.1@gmail.com>
2021-08-27 10:43:13 -0400

    Converted helpers to pip download instead of local file

--- Streamlit Apps/NewDataApp.py
+++ Streamlit Apps/NewDataApp.py
@@ -1,11 +1,8 @@
+from ramsey import data_collect
 from pytube import Playlist
 import streamlit as st
 import pyodbc as sql
 import pandas as pd
-import sys
-
-sys.path.append(r'C:\Users\Samuel\Google Drive\Portfolio\Ramsey')
-import ramsey_collectors as RC
 
 st.set_page_config(layout='wide')
 st.title('The Ramsey Highlights')
@@ -16,25 +13,22 @@ username = login.text_input('Username:', 'guest_login')
 password = login.text_input('Password:', 'ReadOnly!23')
 submit = login.form_submit_button()
 
-if submit:
-    login.empty()
-
 options = st.sidebar.radio('', ['All Data', 'Link'])
 
 if options == 'Link':
     video_link = st.sidebar.text_input('Link:', 'https://www.youtube.com/watch?v=NpKZTVehzbE')
-    audio_location = st.sidebar.text_input('Audio Path:', r'C:\Users\Samuel\Audio\Audio Full\Ramsey')
-    transcript_location = st.sidebar.text_input('Transcript Path:', r'C:\Users\Samuel\Audio\Transcript\Ramsey')
+    audio_location = st.sidebar.text_input('Audio Path:', r'C:\Users\Samuel\Google Drive\Portfolio\Ramsey\Audio\Audio Full\Ramsey')
+    transcript_location = st.sidebar.text_input('Transcript Path:', r'C:\Users\Samuel\Google Drive\Portfolio\Ramsey\Audio\Transcript\Ramsey')
     
     run = st.sidebar.button('BEGIN COLLECTION')
     
     if run:
-        RC.data_collect(video_link, username, password, audio_location, transcript_location)
+        data_collect(video_link, username, password, audio_location, transcript_location)
 
 if options == 'All Data':
     personality = st.sidebar.text_input('Personality:', 'Ramsey')
-    audio_location = st.sidebar.text_input('Audio Path:', f'C:\\Users\\Samuel\\Audio\\Audio Full\\{personality}')
-    transcript_location = st.sidebar.text_input('Transcript Path:', f'C:\\Users\\Samuel\\Audio\\Transcript\\{personality}')
+    audio_location = st.sidebar.text_input('Audio Path:', f'C:\\Users\\Samuel\\Google Drive\\Portfolio\\Ramsey\\Audio\\Audio Full\\{personality}')
+    transcript_location = st.sidebar.text_input('Transcript Path:', f'C:\\Users\\Samuel\\Google Drive\\Portfolio\\Ramsey\\Audio\\Transcript\\{personality}')
     
     personalities = {'ramsey': 'https://www.youtube.com/watch?v=0JUw1agDjoA&list=UU7eBNeDW1GQf2NJQ6G6gAxw&index=2',
                      'deloney': 'https://www.youtube.com/watch?v=_wWc1Tc19qA&list=UU4HiMKM8WLcNbt9ae_XNRNQ&index=2',
@@ -61,7 +55,7 @@ if options == 'All Data':
             collected = pd.read_sql(query, con)
             con.close()
             
-            videos = set(videos) - set(collected['link'])
+            videos = list(set(videos) - set(collected['link']))
             
         if len(videos) <= 0:
             st.write('No New Videos to Upload')
@@ -76,17 +70,21 @@ if options == 'All Data':
                 pb_meta.progress((i+1)/len(videos))
                 i += 1
                 
-                out = RC.data_collect(video, username, password, audio_location, transcript_location, verbose=False)
+                try:
+                    out = data_collect(video, username, password, audio_location, transcript_location, verbose=False)
                 
-                if first:
-                    meta_frame = out[0]
-                    audio_frame = out[1]
-                    
-                    first = False
-                    
-                else:
-                    meta_frame = meta_frame.append(out[0], ignore_index=True, sort=False)
-                    audio_frame = audio_frame.append(out[1], ignore_index=True, sort=False)
+                    if first:
+                        meta_frame = out[0]
+                        audio_frame = out[1]
+                        
+                        first = False
+                        
+                    else:
+                        meta_frame = meta_frame.append(out[0], ignore_index=True, sort=False)
+                        audio_frame = audio_frame.append(out[1], ignore_index=True, sort=False)
+                        
+                except:
+                    st.write(f'Broken: {video}')
                 
             st.write('Meta Data')
             st.dataframe(meta_frame)
