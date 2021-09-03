cececa722f710687a9bbe2e5bf75cbf3f2221a3f
Zangorth <Samuel.A.York.1@gmail.com>
2021-09-03 08:41:38 -0400

    Updated query for speed

--- Streamlit Apps/TrainingApp.py
+++ Streamlit Apps/TrainingApp.py
@@ -1,11 +1,11 @@
 from urllib.request import urlopen
-from numpy.random import randint
 from pydub.playback import play
 from pydub import AudioSegment
 from io import BytesIO
 import streamlit as st
 import pyodbc as sql
 import pandas as pd
+import numpy as np
 
 personalities = ['Ramsey', 'Deloney', 'Coleman', 'AO', 'Cruze', 'Wright', 'Kamel']
 api = 'AIzaSyAftHJhz8-5UUOACb46YBLchKL78yrXpbw'
@@ -41,20 +41,14 @@ begin = st.sidebar.button('BEGIN TRAINING')
 
 if begin:
     query = f'''
-    SELECT audio.channel, audio.id, [second],
-        YEAR(publish_date) AS 'year', link, drive
-    FROM
-        (SELECT channel, id, MAX(second) AS [second]
-        FROM ramsey.audio
-        WHERE audio.channel IN {channel_filter}
-        GROUP BY channel, id) audio
-    LEFT JOIN ramsey.metadata
-        ON metadata.channel = audio.channel
-        AND metadata.id = audio.id
-    WHERE {year_filter}
+    SELECT * 
+    FROM ramsey.metadata
+    WHERE seconds <= 900
+        AND channel IN {channel_filter}
+        AND {year_filter}
     '''
     
-    with st.spinner('Reading Data from Azure... This could take several minutes...'):
+    with st.spinner('Reading Data from Azure'):
         connection_string = ('DRIVER={ODBC Driver 17 for SQL Server};' + 
                               'Server=zangorth.database.windows.net;DATABASE=HomeBase;' +
                               f'UID={username};PWD={password}')
@@ -63,29 +57,46 @@ if begin:
         checked = pd.read_sql('SELECT * FROM ramsey.training', con)
         con.close()
         
-        collected = collected.sample(frac=1).reset_index(drop=True)
+        
+        
+        collected = collected.sample(100000, replace=True, weights=collected['seconds']).reset_index(drop=True)
         
         st.session_state['panda'] = collected
         st.session_state['i'] = 0
+        st.session_state['seed'] = np.random.randint(1, 1000000)
+        st.session_state['sound'] = True
         
         
 if 'panda' in st.session_state:
     i = st.session_state['i']
+    st.session_state['old_eye'] = i
+    rng = np.random.default_rng(st.session_state['seed']+i)
+    
     personality = st.session_state['panda']['channel'][i]
     sample = st.session_state['panda']['id'][i]
-    second = randint(0, st.session_state['panda']['second'][i]-1)
+    second = rng.integers(0, st.session_state['panda']['seconds'][i]-1)
     link = st.session_state['panda']['link'][i]
     drive = st.session_state['panda']['drive'][i]
     
-    sound_byte = BytesIO(urlopen(f'https://www.googleapis.com/drive/v3/files/{drive}?key={api}&alt=media').read())
-    sound = AudioSegment.from_file(sound_byte)
+    if st.session_state['sound']:
+        sound_byte = BytesIO(urlopen(f'https://www.googleapis.com/drive/v3/files/{drive}?key={api}&alt=media').read())
+        st.session_state['sound'] = AudioSegment.from_file(sound_byte)
     
+    sound = st.session_state['sound']
     lead = sound[second*1000-3000: second*1000+3000]
     sound = sound[second*1000:second*1000+1000]
     
     play(sound)
     
-    context = st.button('CONTEXT')
+    st.subheader(f'Iteration {i}: {personality} {sample} - Second {second}')
+    
+    left, middle, right = st.columns(3)
+    
+    replay = left.button('REPLAY')
+    context = middle.button('CONTEXT')
+    
+    if replay:
+        play(sound)
     
     if context:
         play(lead)
@@ -93,87 +104,30 @@ if 'panda' in st.session_state:
     if 'speaker' in models and 'gender' in models:
         upload_form = st.form('upload_both', clear_on_submit=True)
         left, right = upload_form.columns(2)
-        speaker_upload = left.radio('Speaker', personalities + ['Hogan', 'None'])
+        speaker_upload = left.radio('Speaker', personalities + ['Hogan', 'Guest', 'None'])
         gender_upload = right.radio('Gender', ['Man', 'Woman', 'None'])
         send = upload_form.form_submit_button()
         
-        st.session_state['i'] += 1
-        
     elif 'speaker' in models:
         upload_form = st.form('upload_speaker', clear_on_submit=True)
-        speaker_upload = upload_form.radio('Speaker', personalities + ['Hogan', 'None'])
+        speaker_upload = upload_form.radio('Speaker', personalities + ['Hogan', 'Guest', 'None'])
         send = upload_form.form_submit_button()
-        
-        st.session_state['i'] += 1
         
     elif 'gender' in models:
         upload_form = st.form('upload_gender', clear_on_submit=True)
-        gender_upload = upload_form.radio('Gender', personalities + ['Hogan', 'None'])
+        gender_upload = upload_form.radio('Gender', ['Man', 'Woman', 'None'])
         send = upload_form.form_submit_button()
         
-        st.session_state['i'] += 1
-        
     if send:
+        # It doesn't always update the i for some reason, so the loop fixes that
+        with st.spinner('Updating Eyes'):
+            while st.session_state['old_eye'] == st.session_state['i']:
+                st.session_state['i'] += 1
+        
+        st.session_state['sound'] = True
         st.write(f'Speaker: {speaker_upload} | Gender: {gender_upload}')
     
     
-    
-    
-    
-        
-    
-        
-        # checked['identified_anywhere'] = 1
-        
-        # collected = collected.merge(checked, on=['channel', 'id', 'second'], how='left')
-        # collected = collected.loc[collected['identified_anywhere'].isnull()]
-        
-        # cols = ['channel', 'id', 'second'] + models
-        
-        # checked = checked[models].dropna()
-        # checked['identified_model'] = 1
-        
-        # collected = collected.merge(checked, on=['channel', 'id', 'second'], how='left')
-
-    
-    # i, stop = -1, False
-    # while not stop:
-    #     i += 1
-        
-    #     personality = collected['channel'][i]
-    #     sample = collected['id'][i]
-    #     second = collected['second'][i]
-    #     link = collected['link'][i]
-        
-    #     sound = AudioSegment.from_file(f'C:\\Users\\Samuel\\Audio\\Audio Full\\{personality}\\{personality}\\{sample} {personality}.mp3')
-    #     lead = sound[second*1000-3000: second*1000+3000]
-    #     sound = sound[second*1000:second*1000+1000]
-
-    #     left, middle, right = st.columns(3)
-        
-    #     ramsey = left.button('Ramsey')
-    #     ao = left.button('AO')
-    #     kamel = left.button('Kamel')
-    #     deloney = middle.button('Deloney')
-    #     cruze = middle.button('Cruze')
-    #     lead = left.button('CONTEXT')
-    #     coleman = right.button('Coleman')
-    #     wright = right.button('Wright')
-    #     close = right.button('Stop Training')
-        
-    #     while not any([ramsey, ao, kamel, deloney, cruze, lead, coleman, wright, close]):
-    #         pass
-
-
-
-
-
-
-
-
-
-
-
 
 
 
