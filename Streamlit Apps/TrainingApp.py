a92fb200e3c779f25af213298704561d0addf23a
Zangorth <Samuel.A.York.1@gmail.com>
2021-09-03 09:16:30 -0400

    Fixed echoing problem

--- Streamlit Apps/TrainingApp.py
+++ Streamlit Apps/TrainingApp.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen
+from helpers.ramsey import upload
 from pydub.playback import play
 from pydub import AudioSegment
+from time import sleep
 from io import BytesIO
 import streamlit as st
 import pyodbc as sql
@@ -64,7 +66,7 @@ if begin:
         st.session_state['panda'] = collected
         st.session_state['i'] = 0
         st.session_state['seed'] = np.random.randint(1, 1000000)
-        st.session_state['sound'] = True
+        st.session_state['sound'] = ''
         
         
 if 'panda' in st.session_state:
@@ -78,15 +80,20 @@ if 'panda' in st.session_state:
     link = st.session_state['panda']['link'][i]
     drive = st.session_state['panda']['drive'][i]
     
-    if st.session_state['sound']:
+    if st.session_state['sound'] == '':
         sound_byte = BytesIO(urlopen(f'https://www.googleapis.com/drive/v3/files/{drive}?key={api}&alt=media').read())
         st.session_state['sound'] = AudioSegment.from_file(sound_byte)
     
-    sound = st.session_state['sound']
-    lead = sound[second*1000-3000: second*1000+3000]
-    sound = sound[second*1000:second*1000+1000]
-    
-    play(sound)
+        sound = st.session_state['sound']
+        lead = sound[second*1000-3000: second*1000+3000]
+        sound = sound[second*1000:second*1000+1000]
+        
+        play(sound)
+        
+    else:
+        sound = st.session_state['sound']
+        lead = sound[second*1000-3000: second*1000+3000]
+        sound = sound[second*1000:second*1000+1000]
     
     st.subheader(f'Iteration {i}: {personality} {sample} - Second {second}')
     
@@ -111,47 +118,29 @@ if 'panda' in st.session_state:
     elif 'speaker' in models:
         upload_form = st.form('upload_speaker', clear_on_submit=True)
         speaker_upload = upload_form.radio('Speaker', personalities + ['Hogan', 'Guest', 'None'])
+        gender_upload = None
         send = upload_form.form_submit_button()
         
     elif 'gender' in models:
         upload_form = st.form('upload_gender', clear_on_submit=True)
+        speaker_upload = None
         gender_upload = upload_form.radio('Gender', ['Man', 'Woman', 'None'])
         send = upload_form.form_submit_button()
         
     if send:
-        # It doesn't always update the i for some reason, so the loop fixes that
-        with st.spinner('Updating Eyes'):
-            while st.session_state['old_eye'] == st.session_state['i']:
-                st.session_state['i'] += 1
+        loadup = pd.DataFrame({'channel': [personality], 'id': [sample], 'second': [second],
+                               'speaker': speaker_upload, 'gender': gender_upload})
         
-        st.session_state['sound'] = True
+        if username == 'zangorth':
+            with st.spinner('Uploading Data to Azure'):
+                upload(loadup, 'training', username, password)
+                
+        else:
+            with st.spinner('Guest Users are not Allowed to Upload Data'):
+                sleep(3)
+        
+        st.session_state['i'] = i + 1
+        st.session_state['sound'] = ''
         st.write(f'Speaker: {speaker_upload} | Gender: {gender_upload}')
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         
-
-
-
+        st.experimental_rerun()
