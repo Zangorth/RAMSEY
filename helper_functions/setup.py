6bb13c067694c12307b808b3b9e99ae1cf8362a3
Zangorth <Samuel.A.York.1@gmail.com>
2021-09-05 11:00:35 -0400

    Moved local option inside function for ease of use

--- helper_functions/setup.py
+++ helper_functions/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name='zangorth-ramsey',
-      version='1.0.0',
+      version='1.0.1',
       description='Helper Functions for Ramsey Project',
       author='Zangorth',
       packages=['ramsey'],
